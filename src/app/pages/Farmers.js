16a4213c41d2153d29f3209f7f1ffaa5379adb68
Nelson M. Omoro <nmomoro@micglobalrisks.com>
2023-07-03 11:36:20 +0300

    added farmer pagination

--- src/app/pages/Farmers.js
+++ src/app/pages/Farmers.js
@@ -1,84 +1,203 @@
-import React, { Component } from "react";
+import React, { useMemo, useState } from "react";
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+  getPaginationRowModel,
+  getSortedRowModel,
+} from "@tanstack/react-table";
+import { farmers } from "../mock_data";
+import { BiSolidUpArrow, BiSolidDownArrow } from "react-icons/bi";
 
-export class Farmers extends Component {
-  data = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
-  render() {
-    return (
-      <div>
-        <div className="row page-title-header">
-          <div className="col-12">
-            <div className="page-header">
-              <h4 className="page-title">Farmers</h4>
-              <div className="quick-link-wrapper w-10 d-md-flex flex-md-wrap">
-                <ul className="quick-links ml-auto">
-                  <li>
-                    <a href="!#" onClick={(evt) => evt.preventDefault()}>
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a href="!#" onClick={(evt) => evt.preventDefault()}>
-                      Analytics
-                    </a>
-                  </li>
-                  <li>
-                    <a href="!#" onClick={(evt) => evt.preventDefault()}>
-                      Watchlist
-                    </a>
-                  </li>
-                </ul>
-              </div>
+const Farmers = () => {
+  const data = useMemo(() => farmers, []);
+
+  const [sorting, setSorting] = useState([]);
+
+  //  {
+  //   firstname: "Michelle",
+  //   secondname: "Wangui",
+  //   phone_number: "+254709876543",
+  //   farmer_county: "Kisii",
+  //   gender: "Female",
+  //   acreage: 1.66,
+  //   offered: 0.0,
+  //   purchased_volume: 0.0,
+  //   traded_volume: 0.0,
+  // },
+
+  const columns = [
+    {
+      header: "Farmer",
+      accessorFn: (row) => `${row.firstname} ${row.secondname}`,
+    },
+    {
+      header: "Phone Number",
+      accessorKey: "phone_number",
+    },
+    {
+      header: "County of Origin",
+      accessorKey: "farmer_county",
+    },
+    {
+      header: "Gender",
+      accessorKey: "gender",
+    },
+    {
+      header: "Acreage",
+      accessorKey: "acreage",
+    },
+    {
+      header: "Offered",
+      accessorKey: "offered",
+    },
+    {
+      header: "Purchased Volume (T)",
+      accessorKey: "purchased_volume",
+    },
+    {
+      header: "Inventory Volume (T)",
+      accessorKey: "inventory_volume",
+    },
+  ];
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+  });
+
+  return (
+    <div>
+      <div className="row page-title-header">
+        <div className="col-12">
+          <div className="page-header">
+            <h4 className="page-title">Agents</h4>
+            <div className="quick-link-wrapper w-10 d-md-flex flex-md-wrap">
+              <ul className="quick-links ml-auto">
+                <li>
+                  <a href="!#" onClick={(evt) => evt.preventDefault()}>
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a href="!#" onClick={(evt) => evt.preventDefault()}>
+                    Analytics
+                  </a>
+                </li>
+                <li>
+                  <a href="!#" onClick={(evt) => evt.preventDefault()}>
+                    Watchlist
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-lg-12 grid-margin stretch-card">
-            <div className="card">
-              <div className="card-body">
-                <h4 className="card-title">Farmer List</h4>
-                <div className="table-responsive">
-                  <table className="table table-striped">
-                    <thead>
-                      <tr>
-                        <th></th>
-                        <th> Farmer Name </th>
-                        <th> Phone Number </th>
-                        <th> Farmer County </th>
-                        <th> Gender </th>
-                        <th> Acreage </th>
-                        <th> Offered (Kgs) </th>
-                        <th> Purchased Volume(T) </th>
-                        <th> Traded Volume(T) </th>
+      </div>
+      <div className="row">
+        <div className="col-lg-12 grid-margin stretch-card">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-title">Agent List</h4>
+              <div className="table-responsive">
+                <table className="table table-striped">
+                  <thead>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                      <tr key={headerGroup.id}>
+                        <th>#</th>
+                        {headerGroup.headers.map((header) => (
+                          <th
+                            key={header.id}
+                            onClick={header.column.getToggleSortingHandler()}
+                          >
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                            &nbsp;
+                            {
+                              {
+                                asc: (
+                                  <BiSolidUpArrow
+                                    size={10}
+                                    style={{ marginTop: "-2%" }}
+                                  />
+                                ),
+                                desc: (
+                                  <BiSolidDownArrow
+                                    size={10}
+                                    style={{ marginTop: "-2%" }}
+                                  />
+                                ),
+                              }[header.column.getIsSorted() ?? null]
+                            }
+                          </th>
+                        ))}
                       </tr>
-                    </thead>
-                    <tbody>
-                      {this.data.map(() => (
-                        <tr>
-                          <td className="py-1">
-                            <img
-                              src={require("../../assets/images/faces/face1.jpg")}
-                              alt="user icon"
-                            />
+                    ))}
+                  </thead>
+                  <tbody>
+                    {table.getRowModel().rows.map((row) => (
+                      <tr key={row.id}>
+                        <td className="py-1">
+                          <img
+                            src={require("../../assets/images/faces/face1.jpg")}
+                            alt="user icon"
+                          />
+                        </td>
+                        {row.getVisibleCells().map((cell) => (
+                          <td key={cell.id}>
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}
                           </td>
-                          <td> Jackson Mwatela </td>
-                          <td>+254742044215</td>
-                          <td> Meru </td>
-                          <td> Male </td>
-                          <td> 1.62 </td>
-                          <td> 0.00 </td>
-                          <td> 0.00 </td>
-                          <td> 0.00 </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
+                        ))}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <br />
+                <button
+                  className="btn btn-outline-secondary"
+                  onClick={() => table.setPageIndex(0)}
+                >
+                  First page
+                </button>
+                <button
+                  className="btn btn-outline-secondary"
+                  onClick={() => table.previousPage()}
+                  disabled={!table.getCanPreviousPage()}
+                >
+                  Prev page
+                </button>
+                <button
+                  className="btn btn-outline-secondary"
+                  onClick={() => table.nextPage()}
+                  disabled={!table.getCanNextPage()}
+                >
+                  Next page
+                </button>
+                <button
+                  className="btn btn-outline-secondary"
+                  onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                >
+                  Last page
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default Farmers;
